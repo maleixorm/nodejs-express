6f3b7dafc77533488e32b69b46bb322b7012651c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-22 11:54:39 -0300

    Criando um middleware na aplicação

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const port = 3000;
 
 const basePath = path.join(__dirname, 'templates');
 
+const checkAuth = function(req, res, next) {
+    if (req.authStatus) {
+        console.log("Está logado. Pode continuar!");
+    } else {
+        console.log("Não está logado! Faça o login para continuar!");
+    }
+}
+
+app.use(checkAuth);
+
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`);
 });
